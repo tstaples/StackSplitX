7bc03034d15ba70ae2197ac86db2799c0aba2c6f
tyler staples <tyler.a.staples@gmail.com>
2016-12-10 20:13:41 -0800

    Adding old project files updated for new SMAPI. Lots of cleanup to do.

---
+++ StackSplitX/StackSplitX.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using StardewValley.Menus;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace StackSplitX
+{
+    public class StackSplitX : Mod
+    {
+        /* 
+            Menus:
+            - inventory
+            - chest
+            - shop
+        */
+
+        private bool isOpen = false;
+        private bool shiftHeld = false;
+        private TInventoryPage myInventoryPage;
+        //private TInventoryMenu myInventoryMenu = null;
+
+        public override void Entry(IModHelper helper)
+        {
+            MenuEvents.MenuChanged += OnMenuChanged;
+            MenuEvents.MenuClosed += OnMenuClosed;
+        }
+
+        private void OnMenuClosed(object sender, EventArgsClickableMenuClosed e)
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                ControlEvents.MouseChanged += OnMouseStateChanged;
+                ControlEvents.KeyPressed += OnKeyPressed;
+                ControlEvents.KeyReleased += OnKeyReleased;
+            }
+        }
+
+        private void OnMenuChanged(object sender, EventArgsClickableMenuChanged e)
+        {
+            DebugPrintMenuInfo(e.PriorMenu, e.NewMenu);
+
+            if (e.PriorMenu != null && e.PriorMenu.GetType() == e.NewMenu.GetType())
+            {
+                Monitor.Log("resize event");
+                return;
+            }
+
+            if (e.NewMenu is InventoryPage)
+            {
+                isOpen = true;
+
+                myInventoryPage = TInventoryPage.ConstructFromBaseClass((InventoryPage)Game1.activeClickableMenu);
+                //InventoryMenu inventoryMenu = Utils.GetNativeField<InventoryMenu, InventoryPage>(inventoryPage, "inventory");
+                //myInventoryMenu = TInventoryMenu.ConstructFromBaseClass(inventoryMenu);
+
+                ControlEvents.MouseChanged += OnMouseStateChanged;
+                ControlEvents.KeyPressed += OnKeyPressed;
+                ControlEvents.KeyReleased += OnKeyReleased;
+            }
+        }
+
+        private void OnKeyReleased(object sender, EventArgsKeyPressed e)
+        {
+            shiftHeld = !(e.KeyPressed == Keys.LeftShift || e.KeyPressed == Keys.RightShift);
+        }
+
+        private void OnKeyPressed(object sender, EventArgsKeyPressed e)
+        {
+            shiftHeld = (e.KeyPressed == Keys.LeftShift || e.KeyPressed == Keys.RightShift);
+        }
+
+        private void OnMouseStateChanged(object sender, EventArgsMouseStateChanged e)
+        {
+            if (e.NewState.RightButton == ButtonState.Released && shiftHeld)
+            {
+                Monitor.Log("hotkey macro pressed");
+            }
+        }
+
+        private void DebugPrintMenuInfo(IClickableMenu priorMenu, IClickableMenu newMenu)
+        {
+#if DEBUG
+            try
+            {
+                string priorName = "None";
+                if (priorMenu != null)
+                {
+                    priorName = priorMenu.GetType().Name;
+                }
+                string newName = newMenu.GetType().Name;
+                Monitor.Log("Menu changed from: " + priorName + " to " + newName);
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log("Error getting menu name: " + ex);
+            }
+#endif
+        }
+    }
+}
---
+++ StackSplitX/Utils.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+
+namespace StackSplitX
+{
+    public class Utils
+    {
+        public static string ArrayToString<T>(T[] array)
+        {
+            string s = "";
+            int i = 0;
+            foreach (T item in array)
+            {
+                s += item.ToString() + ((++i < array.Length) ? ", " : "");
+            }
+            return s;
+        }
+
+        public static T[] ConcatArrays<T>(T[] a, T[] b)
+        {
+            T[] c = new T[a.Length + b.Length];
+            Array.Copy(a, c, a.Length);
+            Array.Copy(b, c, b.Length);
+            return c;
+        }
+
+        public static int[] StringToIntArray(string[] array, int defaultVal=0)
+        {
+            int[] output = new int[array.Length];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == null || !array[i].IsInt32())
+                    continue;
+
+                try
+                {
+                    output[i] = Int32.Parse(array[i]);
+                }
+                catch (Exception)
+                {
+                    output[i] = defaultVal;
+                }
+            }
+            return output;
+        }
+
+        public static T GetNativeField<T, Instance>(Instance instance, string fieldName)
+        {
+            FieldInfo fieldInfo = typeof(Instance).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return (T)fieldInfo.GetValue(instance);
+        }
+
+        public static FieldInfo[] GetNativeFields<T>()
+        {
+            return typeof(T).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        public static FieldInfo GetNativeFieldInfoByName<T>(string name)
+        {
+            return typeof(T).GetField(name, BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        public static bool IsType<T>(object o)
+        {
+            if (o != null)
+            {
+                return (o.GetType() == typeof(T));
+            }
+            return false;
+        }
+
+        public static int GetTileSheetIndexFromID(int id)
+        {
+            if (id == 0)
+                return 0;
+
+            const int spriteSize = 16; // each sprite on this sheet is 16x16
+            int x = (int)Math.Floor((float)(id / 24.0f));
+            int y = id % spriteSize;
+            return (y * spriteSize) + x;
+        }
+
+        public static int Clamp(int val, int min, int max)
+        {
+            return Math.Max(Math.Min(val, max), min);
+        }
+    }
+}
